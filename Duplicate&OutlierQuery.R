f710de2549068c9872877980e33c2290ece08401
BROWN Daniel <Daniel.T.BROWN@deq.oregon.gov>
2026-01-16 12:16:58 -0800

    Updated date range for Q1 2026

--- Duplicate&OutlierQuery.R
+++ Duplicate&OutlierQuery.R
@@ -31,8 +31,8 @@ setwd("//deqlab1/Assessment/AWQMS/Validation")
 ### Set the data window by changing these dates
 ### For quarterly audits, set the Q_date range to one year, then run lines 48-51
 ### For pre-Integrated Report audits, set the IR_date range to five years, then run lines 54-56
-Q_Start_Date <- '2024-09-23'
-Q_End_Date <- '2025-09-23'
+Q_Start_Date <- '2025-01-16'
+Q_End_Date <- '2026-01-15'
 
 # IR_Start_Date <- '2020-01-01'
 # IR_End_Date <- '2024-12-31'
